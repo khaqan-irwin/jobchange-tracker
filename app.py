0df6d13eb901325561933c51e33e5e84cfd93354
khaqan-irwin <khaqan@getirwin.com>
2025-01-08 12:38:01 +0300

    Threshold set 50

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def linkedin_profile_url_validation(dataset):
     validated_dataset = dataset[dataset['LinkedIn Profile'].apply(lambda url: bool(re.match(linkedin_regex, url))) ]
     return validated_dataset
 
-def is_similar_company(company1, company2, threshold=60):
+def is_similar_company(company1, company2, threshold=52):
     similarity_score = ratio(company1.lower(), company2.lower()) * 100
     return similarity_score >= threshold
 
